e8252d1920d8d5d69795b43273763268fea0f9df
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-23 02:36:05 +0600

    Стили css классы

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import Counter from './components/Counter';
+import React, { useState } from 'react'
+import './styles/App.css'
 
 function App() {
-  
-
-
-
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='post'>
+				<div className='post__content'>
+					<strong>JavaScript</strong>
+					<div>JavaScript - язык программирования</div>
+				</div>
+				<div className='post__btns'>
+					<button>Delete</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
