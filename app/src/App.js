a5a1dc76a307e1b6e1c81ae5d21d1e0b0ec2597b
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-23 03:25:50 +0600

    Преобразование массива объектов в массив React элементов

--- app/src/App.js
+++ app/src/App.js
@@ -3,11 +3,18 @@ import PostItem from './components/PostItem'
 import './styles/App.css'
 
 function App() {
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'js', body: 'DESCRIPTION' },
+		{ id: 2, title: 'java', body: 'DESCRIPTION' },
+		{ id: 3, title: 'c#', body: 'DESCRIPTION' },
+		{ id: 4, title: 'c++', body: 'DESCRIPTION' },
+	])
+
 	return (
 		<div className='App'>
-			<PostItem
-				post={{ id: 1, title: 'javascript', body: 'Description' }}
-			></PostItem>
+			<h1 style={{ textAlign: 'center' }}>Список постов</h1>
+			{posts.map(post => <PostItem post={post} key={post.id} />
+			)}
 		</div>
 	)
 }
