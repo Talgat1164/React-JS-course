b2daec8e305dd52d2943f1c8e164d97741501666
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-23 01:18:46 +0600

    Управляемый инпут

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('')
 
   function increment() {
     setLikes(likes + 1); 
@@ -16,6 +17,14 @@ function App() {
       <h1>Likes: {likes}</h1>
       <button onClick={increment}>+1</button>
       <button onClick={dicrement}>-1</button>
+
+      <h1>Text from input: {value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      /> 
+      
     </div>
   );
 }
