a38f2b399cd3aa27300f238c18084fb39c649740
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-23 01:12:31 +0600

    Use state

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [likes, setLikes] = useState(0)
+
+  function increment() {
+    setLikes(likes + 1); 
+  } 
+
+  function dicrement() {
+    setLikes(likes - 1); 
+  } 
+
   return (
     <div className="App">
-
+      <h1>Likes: {likes}</h1>
+      <button onClick={increment}>+1</button>
+      <button onClick={dicrement}>-1</button>
     </div>
   );
 }
