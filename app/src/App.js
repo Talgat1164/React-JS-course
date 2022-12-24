58ccd75dec15c33c2c903312753cd0d1fdec775d
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-24 22:29:28 +0600

    axios

--- app/src/App.js
+++ app/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useMemo, useState } from 'react'
 import PostFilter from './components/PostFilter'
 import PostForm from './components/PostForm'
@@ -21,6 +22,11 @@ function App() {
 		setModal(false)
 	}
 
+	async function fetchPosts() {
+		const response = await axios.get('https://jsonplaceholder.typicode.com/posts/')
+		setPosts(response.data)
+	}
+
 	// Получаем пост из дочернего компонента
 	const removePost = (post) => {
 		setPosts(posts.filter(p => p.id !== post.id))
@@ -29,6 +35,7 @@ function App() {
 	
 	return (
 		<div className='App'>
+			<button onClick={fetchPosts}>Get posts</button>
 			<MyButton style={{margin: '15px 0'}} onClick={() => setModal(true)}>
 				Создать пост
 			</MyButton>
