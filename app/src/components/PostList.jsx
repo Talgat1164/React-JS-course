d4370f06b9f0ff2f9c9284079d71577b46b35f3e
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-24 22:11:27 +0600

    Анимации. React transition group

--- app/src/components/PostList.jsx
+++ app/src/components/PostList.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import PostItem from './PostItem'
+import React from 'react';
+import PostItem from "./PostItem";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
-function PostList({posts, title, remove}) {
-	if(!posts.length) { 
-		return <h1 style={{textAlign: 'center'}}>Посты не найдены</h1> 
+const PostList = ({posts, title, remove}) => {
 
-	} 
+    if (!posts.length) {
+        return (
+            <h1 style={{textAlign: 'center'}}>
+                Посты не найдены!
+            </h1>
+        )
+    }
 
-	return (
-		<div>
-			<h1 style={{ textAlign: 'center' }}>{title}</h1>
-			{posts.map((post, index) => <PostItem
-				remove={remove}
-				number={index + 1}  
-				post={post} 
-				key={post.id} 
-			/>)}
-		</div>
-	)
-}
+    return (
+        <div>
+            <h1 style={{textAlign: 'center'}}>
+                {title}
+            </h1>
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
+        </div>
+    );
+};
 
-export default PostList
+export default PostList;
