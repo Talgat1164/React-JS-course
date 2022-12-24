6685e13b7e717c343f4f8563d441ac57c8d0e0fb
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-24 22:19:45 +0600

    Создали хук с филтрацик и сортировкой

--- app/src/App.js
+++ app/src/App.js
@@ -6,30 +6,15 @@ import MyButton from './components/UI/button/MyButton'
 import MyInput from './components/UI/input/MyInput'
 import MyModal from './components/UI/MyModal/MyModal'
 import MySelect from './components/UI/MySelect/MySelect'
+import { usePosts } from './hooks/usePosts'
 import './styles/App.css'
 
 function App() {
-	const [posts, setPosts] = useState([
-		{ id: 1, title: 'js', body: 'DESCRIPTION' },
-		{ id: 2, title: 'java', body: 'DESCRIPTION' },
-		{ id: 3, title: 'c#', body: 'DESCRIPTION' },
-		{ id: 4, title: 'c++', body: 'DESCRIPTION' },
-	])
+	const [posts, setPosts] = useState([])
 	
 	const [filter, setFilter] = useState({sort: '', query: ''})
 	const [modal, setModal] = useState(false)
-
-	const sortedPosts = useMemo(() => {
-		console.log('отраб')
-		if (filter.sort) {
-			return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-		}
-		return posts
-	}, [filter.sort, posts])
-
-	const sortedAndSearchedPosts = useMemo(() => {
-		return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-	}, [filter.query, sortedPosts]) 
+	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost])
