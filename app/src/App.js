e9e8f4a56aabd33af744a6523177fc2d98ac38c1
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-25 02:03:28 +0600

    Сделали пагинацию

--- app/src/App.js
+++ app/src/App.js
@@ -12,18 +12,29 @@ import MySelect from './components/UI/MySelect/MySelect'
 import { useFetching } from './hooks/useFetching'
 import { usePosts } from './hooks/usePosts'
 import './styles/App.css'
+import { getPageCount, getPagesArray } from './utils/pages'
 
 function App() {
 	const [posts, setPosts] = useState([])
 	
 	const [filter, setFilter] = useState({sort: '', query: ''})
 	const [modal, setModal] = useState(false)
+	const [totalPages, setTotalPages] = useState(0); 
+	const [limit, setLimit] = useState(10)
+	const [page, setPage] = useState(1)
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-	const [isPostsLoading, setIsPostsLoading] = useState(false) 
-	const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-		 const posts = await PostService.getAll()
-		setPosts(posts)
+	
+	let pagesArray = getPagesArray(totalPages); 
+	
+	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+		const response = await PostService.getAll(limit, page)
+		setPosts(response.data)
+		console.log(response.headers['x-total-count'])
+		const totalCount = response.headers['x-total-count']
+		setTotalPages(getPageCount(totalCount, limit))
 	})
+
+	console.log(totalPages)
 
 	useEffect(() => {
 		fetchPosts()
@@ -42,6 +53,10 @@ function App() {
 		setPosts(posts.filter(p => p.id !== post.id))
 	}
 
+	const changePage = (page) => {
+		setPage(page)
+		fetchPosts()
+	}
 	
 	return (
 		<div className='App'>
@@ -64,6 +79,17 @@ function App() {
 				? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
 				: <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" /> 
 			}
+			<div className="page__wrapper">
+				{pagesArray.map(p => 
+					<span 
+						onClick={() => changePage(p)}
+						key={p}
+						className={page === p ? 'page page__current' : 'page'}
+
+					>{p}</span>
+				)}
+			</div>
+			
 			
 		</div>
 	)
