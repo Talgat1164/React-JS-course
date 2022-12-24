4f7b30b631b3f24864f1ad1c76eac75d89b02b25
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-25 00:01:17 +0600

    Кастомный хук useFetching(). Обработка ошибок

--- app/src/App.js
+++ app/src/App.js
@@ -9,6 +9,7 @@ import MyInput from './components/UI/input/MyInput'
 import Loader from './components/UI/Loader/Loader'
 import MyModal from './components/UI/MyModal/MyModal'
 import MySelect from './components/UI/MySelect/MySelect'
+import { useFetching } from './hooks/useFetching'
 import { usePosts } from './hooks/usePosts'
 import './styles/App.css'
 
@@ -19,6 +20,10 @@ function App() {
 	const [modal, setModal] = useState(false)
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 	const [isPostsLoading, setIsPostsLoading] = useState(false) 
+	const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+		 const posts = await PostService.getAll()
+		setPosts(posts)
+	})
 
 	useEffect(() => {
 		fetchPosts()
@@ -31,14 +36,6 @@ function App() {
 		setModal(false)
 	}
 
-	async function fetchPosts() {
-		setIsPostsLoading(true)
-		setTimeout(async () => {
-			const posts = await PostService.getAll()
-			setPosts(posts)
-			setIsPostsLoading(false)	
-		}, 1000)
-	}
 
 	// Получаем пост из дочернего компонента
 	const removePost = (post) => {
@@ -59,6 +56,9 @@ function App() {
 				filter={filter} 
 				setFilter={setFilter}		
 			/>
+			{postError && 
+				<h1>Произошла ошибка {postError}</h1>
+			}
 			{
 				isPostsLoading
 				? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
