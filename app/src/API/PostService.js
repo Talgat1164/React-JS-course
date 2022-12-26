9f00bf690b1b5eeb576a72d9570932ab64920356
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-27 00:34:45 +0600

    Загрузка комментариев к посту

--- app/src/API/PostService.js
+++ app/src/API/PostService.js
@@ -13,5 +13,9 @@ export default class PostService {
     static async getById(id) {
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts' + id)
         return response;
-  }
+    }
+    static async getCommentsByPostId(id) {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+        return response;
+    }
 }
--- app/src/pages/PostIdPage.jsx
+++ app/src/pages/PostIdPage.jsx
@@ -1,35 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import PostService from '../API/PostService';
-import Loader from '../components/UI/Loader/Loader';
-import { useFetching } from '../hooks/useFetching';
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {useFetching} from "../hooks/useFetching";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/Loader/Loader";
 
-function PostIdPage() {
-	const params = useParams()
-	
-	const [post, setPosts] = useState({});
-	const [fetchPostById, isLoading, error] = useFetching(async(id) => {
-		const response = await PostService.getById(id)
-		setPosts(response.data)
-	})
-	
-	
-	useEffect(() => {
-		fetchPostById(params.id)
-	})
+const PostIdPage = () => {
+    const params = useParams()
+    const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(id)
+        setPost(response.data);
+    })
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id)
+        setComments(response.data);
+    })
 
-	return (
-		<div>
-			<h1>Вы открыли стр поста с ID = {params.id}</h1>
-			{isLoading
-				? <Loader />
-				: <div>{post.id}. {post.title}</div>
-			}
-			<h1>
-				Комментарии
-			</h1>
-		</div>
-	)
-}
+    useEffect(() => {
+        fetchPostById(params.id)
+        fetchComments(params.id)
+    }, [])
 
-export default PostIdPage
+    return (
+        <div>
+            <h1>Вы открыли страницу поста c ID = {params.id}</h1>
+            {isLoading
+                ? <Loader/>
+                :  <div>{post.id}. {post.title}</div>
+            }
+            <h1>
+                Комментарии
+            </h1>
+            {isComLoading
+                ? <Loader/>
+                : <div>
+                    {comments.map(comm =>
+                        <div key={comm.id} style={{marginTop: 15}}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
+            }
+        </div>
+    );
+};
+
+export default PostIdPage;
